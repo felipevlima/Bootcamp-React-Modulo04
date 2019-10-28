caf53493450d04ba8a37e1bb3fb7e1f55a376288
Felipe Vieira <felip.3lima@hotmail.com>
2019-10-28 19:43:59 -0300

    Modulo04 - Aula: Ciclo de vida de um componente

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './App';
+
+render(<App />, document.getElementById('app'));
+
+
+
+/**
+ * Settings.json | Webpack
+ */
+// --mode pruduction
+// --mode development
